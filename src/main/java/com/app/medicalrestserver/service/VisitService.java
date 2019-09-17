92779022d294c0d371bbdd449c04206ac3ebedf6
koondzioo <koleksi1@mion.elka.pw.edu.pl>
2019-09-17 19:54:59 +0200

    small clean constructor, imports etc.

--- src/main/java/com/app/medicalrestserver/service/VisitService.java
+++ src/main/java/com/app/medicalrestserver/service/VisitService.java
@@ -6,8 +6,6 @@ import com.app.medicalrestserver.exceptions.MyException;
 import com.app.medicalrestserver.model.Doctor;
 import com.app.medicalrestserver.model.Patient;
 import com.app.medicalrestserver.model.Visit;
-import com.app.medicalrestserver.repository.DoctorRepository;
-import com.app.medicalrestserver.repository.PatientRepository;
 import com.app.medicalrestserver.repository.VisitRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 
@@ -28,27 +25,22 @@ public class VisitService {
 
     private VisitRepository visitRepository;
     private PatientService patientService;
-    private DoctorRepository doctorRepository;
-    private PatientRepository patientRepository;
     private DoctorService doctorService;
     private ModelMapper modelMapper;
     private static Logger logger = LoggerFactory.getLogger(VisitService.class);
 
 
     @Autowired
-    public VisitService(VisitRepository visitRepository, PatientService patientService, DoctorRepository doctorRepository, PatientRepository patientRepository, DoctorService doctorService, ModelMapper modelMapper) {
+    public VisitService(VisitRepository visitRepository, PatientService patientService, DoctorService doctorService, ModelMapper modelMapper) {
         this.visitRepository = visitRepository;
         this.patientService = patientService;
-        this.doctorRepository = doctorRepository;
-        this.patientRepository = patientRepository;
         this.doctorService = doctorService;
         this.modelMapper = modelMapper;
     }
 
-    public VisitDto addVisit(VisitDto visitDto)
-    {
-        try{
-            if(visitDto == null &&  visitDto.getId() != null){
+    public VisitDto addVisit(VisitDto visitDto) {
+        try {
+            if (visitDto == null && visitDto.getId() != null) {
                 throw new NullPointerException("VISIT OBJECT IS NOT CORRECT");
             }
             Doctor doctor = modelMapper.fromDoctorDtoToDoctor(doctorService.findDoctorById(visitDto.getDoctorId()));
@@ -58,7 +50,7 @@ public class VisitService {
             visit.setPatient(patient);
             visitRepository.save(visit);
             return modelMapper.fromVisitToVisitDto(visit);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error(e.getMessage());
             e.printStackTrace();
             throw new MyException("CREATE VISIT EXCEPTION");
@@ -66,12 +58,12 @@ public class VisitService {
 
     }
 
-    public VisitDto deleteVisit(Long id){
-        try{
+    public VisitDto deleteVisit(Long id) {
+        try {
             Visit visit = visitRepository.findById(id).orElseThrow(NullPointerException::new);
             visitRepository.delete(visit);
             return modelMapper.fromVisitToVisitDto(visit);
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error(e.getMessage());
             throw new MyException("DELETE VISIT EXCEPTION");
         }
@@ -79,10 +71,9 @@ public class VisitService {
 
     public List<VisitDto> getVisitsByPatient(Long id) {
         try {
-            Patient patient =  patientRepository.getOne(id);
-            System.out.println(patient);
+            Patient patient = modelMapper.fromPatientDtoToPatient(patientService.findPatientById(id));
             return patient.getVisitList().stream().map(modelMapper::fromVisitToVisitDto).collect(Collectors.toList());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             logger.error(e.getMessage());
             throw new MyException("GET VISITS BY PATIENT");
@@ -92,7 +83,7 @@ public class VisitService {
     public VisitDto getVisitById(Long id) {
         try {
             return modelMapper.fromVisitToVisitDto(visitRepository.findById(id).orElseThrow(NullPointerException::new));
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error(e.getMessage());
             throw new MyException("GET VISIT BY ID");
         }
